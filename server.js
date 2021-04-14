f4b00215bcf86e2c7dff693d376f64858dbd3020
Derek Ensign <derekensign@gmail.com>
2021-04-14 16:27:01 -0500

    added replaceinfile

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+const replaceinfile = require('replace-in-file')
+
+const path = require('path')
+
+app.get('/', (req, res) => {
+  const filepath = path.join(__dirname, 'index.html')
+  res.sendFile(filepath)
+})
+
+app.get('/main.js', (req, res) => {
+  const filepath = path.join(__dirname, 'main.js')
+  if (process.env.NODE_ENV === 'production') {
+    replaceinfile({
+      files: filepath,
+      from: 'http://localhost:3001',
+      to: ''
+    })
+  }
+  res.sendFile(filepath)
+})
+
+app.get('/style.css', (req, res) => {
+  const filepath = path.join(__dirname, 'style.css')
+  res.type('css').sendFile(filepath)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    routesReport.print()
+})
