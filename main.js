dcd6a9f7f9a7484ef16937276ff9b7bb5ebe36d8
Nick Link <nlink2021@gmail.com>
2021-04-15 15:44:52 -0500

    business forms

--- main.js
+++ main.js
@@ -2,28 +2,41 @@ const homeNav = document.querySelector('#home-link')
 const businessNav = document.querySelector('#business-link')
 const signNav = document.querySelector('#signup-link')
 const loginNav = document.querySelector('#login-link')
+const addbizNav = document.querySelector('#addbiz-link')
 
 const loginContainer = document.querySelector('.loginContainer')
 const searchContainer = document.querySelector('.searchContainer')
 const signupContainer = document.querySelector('.signupContainer')
+const addbizContainer = document.querySelector('.listBizContainer')
 
 const toLogin = () => {
     loginContainer.classList.remove('hidden')
     searchContainer.classList.add('hidden')
     signupContainer.classList.add('hidden')
+    addbizContainer.classList.add('hidden')
 }
 
 const toHome = () => {
     loginContainer.classList.add('hidden')
     signupContainer.classList.add('hidden')
     searchContainer.classList.remove('hidden')
+    addbizContainer.classList.add('hidden')
 }
 
 const toSignup = () => {
     loginContainer.classList.add('hidden')
     signupContainer.classList.remove('hidden')
     searchContainer.classList.add('hidden')
+    addbizContainer.classList.add('hidden')
 }
+
+const toAddBiz = () => {
+    loginContainer.classList.add('hidden')
+    signupContainer.classList.add('hidden')
+    searchContainer.classList.add('hidden')
+    addbizContainer.classList.remove('hidden')
+}
+
 
 
 homeNav.addEventListener('click', (event) => {
@@ -36,4 +49,8 @@ loginNav.addEventListener('click', (event) => {
 
 signNav.addEventListener('click', (event) => {
     toSignup();
+})
+
+addbizNav.addEventListener('click', (event) => {
+    toAddBiz();
 })
