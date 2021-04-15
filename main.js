7781d8612f7537b79fc0a61f34fe417188473d77
Jason Ouyang <jasonouyang112@gmail.com>
2021-04-15 14:05:21 -0700

    getting latest updates

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ const addbizNav = document.querySelector('#addbiz-link')
 const loginContainer = document.querySelector('.loginContainer')
 const searchContainer = document.querySelector('.searchContainer')
 const signupContainer = document.querySelector('.signupContainer')
-<<<<<<< HEAD
 const businessesContainer = document.querySelector('.businessesContainer')
 const allSections = document.querySelectorAll('.section');
 
@@ -19,29 +18,6 @@ const showSection = (sectionsToHide, sectionToShow) => {
     sectionsToHide.forEach(section => section.classList.add('hidden'))
 
     sectionToShow.classList.remove('hidden')
-=======
-const addbizContainer = document.querySelector('.listBizContainer')
-
-const toLogin = () => {
-    loginContainer.classList.remove('hidden')
-    searchContainer.classList.add('hidden')
-    signupContainer.classList.add('hidden')
-    addbizContainer.classList.add('hidden')
-}
-
-const toHome = () => {
-    loginContainer.classList.add('hidden')
-    signupContainer.classList.add('hidden')
-    searchContainer.classList.remove('hidden')
-    addbizContainer.classList.add('hidden')
-}
-
-const toSignup = () => {
-    loginContainer.classList.add('hidden')
-    signupContainer.classList.remove('hidden')
-    searchContainer.classList.add('hidden')
-    addbizContainer.classList.add('hidden')
->>>>>>> b0dd563b6d616d23483e5aaa331f820a4fcf247c
 }
 
 const toAddBiz = () => {
@@ -63,18 +39,9 @@ loginNav.addEventListener('click', (event) => {
 })
 
 signNav.addEventListener('click', (event) => {
-<<<<<<< HEAD
     showSection(allSections,signupContainer)
 })
 
 businessNav.addEventListener('click', (event) => {
     showSection(allSections,businessesContainer)
 });
-=======
-    toSignup();
-})
-
-addbizNav.addEventListener('click', (event) => {
-    toAddBiz();
-})
->>>>>>> b0dd563b6d616d23483e5aaa331f820a4fcf247c
