faff71684fa9769a74fa2165e4f7107e3a03c164
Jason Ouyang <jasonouyang112@gmail.com>
2021-04-15 21:03:56 -0700

    create user

--- userRequests.js
+++ userRequests.js
@@ -1,24 +1,49 @@
 const apiLink = 'http://localhost:3001';
 
-console.log('hello from user')
 
-const getBusinesses = async () => {
+// Creates a user token to be set in local storage
+const createUser = async( name, email, password) => {
     try {
-        const response = await axios.get(`${apiLink}/business`);
-        const businesses = response.data.businesses;
+        const response = await axios.post(`${apiLink}/user`, {
+            name,
+            email,
+            password
+        });
 
-        return businesses;
+        localStorage.setItem('userToken', response.data.userToken);
     }
     catch(error) {
+        console.log(error);
+    }
+};
 
+// Verify user 
+const verifyUser = async() => {
+    const userToken = localStorage.getItem('userToken');
+    try {
+        if (userToken !== null) {
+            const response = await axios.get(`${apiLink}/user/verify`, {
+                headers: {
+                    authorization: 'Bearer ' + userToken 
+                }
+            });
+   
+            if (response.data.message === 'Authenticated') {
+            //    Change Log In State to true.
+            
+            }   
+
+            else {
+
+            }
+    
+        }
+      
+    }
+
+    catch(error) {
+        console.log(error);
     }
 }
-
-
-const createTable = (parentElement, businesses) => {
-    console.log(parentElement)
-
-    const table = document.createElement('table');
-
-    parentElement.append(table)
-}
+// createUser('jason','jason@mail.com','12345');
+// verifyUser();
